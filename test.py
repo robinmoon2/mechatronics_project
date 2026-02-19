f4e16da3bd515f7bc636c4d781417f7d7e344b11
Adeept Robot <robot@mechatronics.com>
2026-02-19 12:37:33 +0100

    test line

--- test.py
+++ test.py
@@ -6,10 +6,39 @@
 
 import time
 from gpiozero import InputDevice
+from board import SCL, SDA
+import busio
+from adafruit_pca9685 import PCA9685
+from adafruit_motor import motor
 
 line_pin_left = 22
 line_pin_middle = 27
 line_pin_right = 17
+
+
+
+
+MOTOR_M1_IN1 =  15      #Define the positive pole of M1
+MOTOR_M1_IN2 =  14      #Define the negative pole of M1
+MOTOR_M2_IN1 =  12      #Define the positive pole of M2
+MOTOR_M2_IN2 =  13      #Define the negative pole of M2
+MOTOR_M3_IN1 =  11      #Define the positive pole of M3
+MOTOR_M3_IN2 =  10      #Define the negative pole of M3
+MOTOR_M4_IN1 =  8       #Define the positive pole of M4
+MOTOR_M4_IN2 =  9       #Define the negative pole of M4
+
+i2c = busio.I2C(SCL, SDA)
+pwm_motor = PCA9685(i2c, address=0x5f)
+pwm_motor.frequency = 50
+
+motor1 = motor.DCMotor(pwm_motor.channels[MOTOR_M1_IN1],pwm_motor.channels[MOTOR_M1_IN2] )
+motor1.decay_mode = (motor.SLOW_DECAY)
+motor2 = motor.DCMotor(pwm_motor.channels[MOTOR_M2_IN1],pwm_motor.channels[MOTOR_M2_IN2] )
+motor2.decay_mode = (motor.SLOW_DECAY)
+motor3 = motor.DCMotor(pwm_motor.channels[MOTOR_M3_IN1],pwm_motor.channels[MOTOR_M3_IN2] )
+motor3.decay_mode = (motor.SLOW_DECAY)
+motor4 = motor.DCMotor(pwm_motor.channels[MOTOR_M4_IN1],pwm_motor.channels[MOTOR_M4_IN2] )
+motor4.decay_mode = (motor.SLOW_DECAY)
 
 left = InputDevice(pin=line_pin_left)
 middle = InputDevice(pin=line_pin_middle)
@@ -20,12 +49,60 @@ def run():
     status_middle = middle.value
     status_left = left.value
     print('left: %d   middle: %d   right: %d' %(status_left,status_middle,status_right))
+    if (status_left == 0) :
+        Motor(2, -1, 10)
+        Motor(1, 1, 40)
+    elif(status_middle == 1)and (status_left == 1) and (status_right == 1):
+        Motor(1, 1, 15)
+        Motor(2, 1, 15)
+    elif (status_right == 0):
+        Motor(1, -1, 10)
+        Motor(2, 1, 40)
+    elif (status_middle == 0) and (status_left == 0) and (status_right == 0):
+        Motor(1, 1, 0)
+        Motor(2, 1, 0)
+
+
+
+def map(x,in_min,in_max,out_min,out_max):
+  return (x - in_min)/(in_max - in_min) *(out_max - out_min) +out_min
+
+def Motor(channel,direction,motor_speed):
+  if motor_speed > 100:
+    motor_speed = 100
+  elif motor_speed < 0:
+    motor_speed = 0
+  speed = map(motor_speed, 0, 100, 0, 1.0)
+  if direction == 1:
+    speed = -speed
+
+  if channel == 1:
+    motor1.throttle = speed
+  elif channel == 2:
+    motor2.throttle = speed
+  elif channel == 3:
+    motor3.throttle = speed
+  elif channel == 4:
+    motor4.throttle = speed
+
+def motorStop():#Motor stops
+    motor1.throttle = 0
+    motor2.throttle = 0
+    motor3.throttle = 0
+    motor4.throttle = 0
+
+def destroy():
+  motorStop()
+  pwm_motor.deinit()
 
 
 if __name__ == '__main__':
     try:
-      while 1:
-        run()
-        time.sleep(0.3)
+        Motor(1, 1, 20)
+        Motor(2, 1, 20)
+        while 1:
+            run()
     except KeyboardInterrupt:
+        destroy()
         pass
+
